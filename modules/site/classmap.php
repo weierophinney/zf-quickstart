09fe4aff42707cc443111e47bbc62f5f046700eb
Evan Coury <me@evancoury.com>
2011-09-09 09:11:24 -0700

    Update module class maps

--- modules/site/classmap.php
+++ modules/site/classmap.php
@@ -1,7 +1,7 @@
 <?php
 return array (
-  'site\\View\\Helper\\Url' => __DIR__ . DIRECTORY_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'Url.php',
-  'site\\Controller\\ErrorController' => __DIR__ . DIRECTORY_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'ErrorController.php',
-  'site\\Controller\\IndexController' => __DIR__ . DIRECTORY_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'IndexController.php',
-  'site\\Information' => __DIR__ . DIRECTORY_SEPARATOR . '.' . DIRECTORY_SEPARATOR . 'Information.php',
+  'site\\Controller\\ErrorController' => __DIR__ . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'ErrorController.php',
+  'site\\Controller\\IndexController' => __DIR__ . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'IndexController.php',
+  'site\\Module' => __DIR__ . DIRECTORY_SEPARATOR . 'Module.php',
+  'site\\View\\Helper\\Url' => __DIR__ . DIRECTORY_SEPARATOR . 'View' . DIRECTORY_SEPARATOR . 'Helper' . DIRECTORY_SEPARATOR . 'Url.php',
 );
